d93891ebc59ad7931b672c5f8846075e08e18044
shirleymango <44847817+shirleymango@users.noreply.github.com>
2019-07-31 15:43:39 -0500

    Update PhotoHub.dart

--- lib/PhotoHub.dart
+++ lib/PhotoHub.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:fresh_hub_app/sunnyside_1.dart';
-import 'package:fresh_hub_app/sunnyside_2.dart';
-import 'package:fresh_hub_app/kashmere_1.dart';
-import 'package:fresh_hub_app/kashmere_2.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'sunnyside_1.dart';
+import 'sunnyside_2.dart';
+import 'kashmere_1.dart';
+import 'kashmere_2.dart';
+import 'sunnyside_3.dart';
+import 'sunnyside_4.dart';
+import 'kashmere_3.dart';
+import 'kashmere_4.dart';
+
+const URL = "https://www.freshhubhouston.com/";
 
 class PhotoHub extends StatelessWidget{
   @override
@@ -23,28 +30,99 @@ Widget getListView(BuildContext context) {
             children: <Widget>[
               new produceImage(),
               VerticalPadding(
-                color: Colors.white,
+                color: Colors.transparent,
               ),
               Text("Fresh Hub",
                 style: TextStyle(fontFamily: 'Abril Fatface', fontSize: 50),
               ),
               VerticalPadding(
-                color: Colors.white,
+                color: Colors.transparent,
               ),
               Text("Mission of Fresh Hub: Our aim is to provide fresh, nutritional food to Houstonians in food deserts.",
                   style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Questrial'),
                   textAlign: TextAlign.center),
               VerticalPadding(
-                color: Colors.white,
+                color: Colors.transparent,
+              ),
+              Text("Click on the button below to visit our website.",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Questrial'),
+                  textAlign: TextAlign.center),
+              Website(),
+              VerticalPadding(
+                color: Colors.transparent,
               ),
               Text("Click on the buttons below to see photos and videos. Go to the navigation bar to check out more cool features.",
                   textAlign: TextAlign.center, style: TextStyle(fontFamily: 'Questrial')),
               VerticalPadding(
-                color: Colors.white,
+                color: Colors.transparent,
               ),
               Text("EVENT PHOTOS", style: TextStyle(decoration: TextDecoration.underline, fontFamily: 'Abril Fatface', fontSize: 30)),
               VerticalPadding(
-                color: Colors.white,
+                color: Colors.transparent,
+              ),
+              // Added 4
+              RaisedButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => kashmere_4()));
+                },
+                child: Text(
+                  "Kashmere 6/15/19",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              // Added 3
+              RaisedButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => sunnyside_4()));
+                },
+                child: Text(
+                  "Sunnyside 5/18/19",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              // Added 2
+              RaisedButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => kashmere_3()));
+                },
+                child: Text(
+                  "Kashmere 4/19/19",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              // Added 1
+              RaisedButton(
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => sunnyside_3()));
+                },
+                child: Text(
+                  "Sunnyside 3/11/19",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
               RaisedButton(
                 color: Colors.green,
@@ -144,7 +222,7 @@ class VerticalPadding extends StatelessWidget {
   VerticalPadding({
     @required this.child,
     this.padding = 8.0,
-    this.color = Colors.white,
+    this.color = Colors.transparent,
   });
   final double padding;
   final Widget child;
@@ -156,6 +234,35 @@ class VerticalPadding extends StatelessWidget {
       color: color,
       padding: EdgeInsets.symmetric(vertical: padding),
       child: child,
+    );
+  }
+}
+
+class Website extends StatefulWidget{
+  @override
+  _WebsiteState createState() => _WebsiteState();
+}
+
+class _WebsiteState extends State<Website>{
+  Future launchURL(String url) async {
+    if (await canLaunch(url))
+      await launch(url, forceSafariVC: true, forceWebView: true);
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children:[
+          RaisedButton(
+            child: Text("Open Link"),
+            color: Colors.blue,
+            splashColor: Colors.orange,
+            onPressed: (){
+              launchURL(URL);
+            },
+          )
+        ]
     );
   }
 }
