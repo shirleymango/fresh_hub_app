b0a811dac3383a88536ac042395871986183ff89
bleh0.5 <qinxiaoying@gmail.com>
2019-08-01 18:44:16 -0500

    Update Map.dart

--- lib/Map.dart
+++ lib/Map.dart
@@ -14,17 +14,26 @@ class Map extends StatefulWidget {
 
 
 class _MapState extends State<Map>{
-
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
   static const LatLng _center = const LatLng(29.656388, -95.3585341);
-  final Set<Marker> _markers = {};
-
+  final Set<Marker> _markers = new Set();
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+  void _onMapTypeButtonPressed(){
+    setState((){
+      if(_currentMapType == MapType.normal) {
+        _currentMapType = MapType.satellite;
+      }
+      else {
+        _currentMapType = MapType.normal;
+      }
+    });
+  }
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
     _onAddMarkerButtonPressed();
   }
-
   void _onAddMarkerButtonPressed() {
     setState(() {
       _markers.add(Marker(
@@ -32,28 +41,58 @@ class _MapState extends State<Map>{
         position: const LatLng(29.656388, -95.3585341),
         infoWindow: InfoWindow(
           title:"Sunnyside Multi-Service Center",
-          snippet: "9314 Cullen   77051",
+          snippet: "9314 Cullen 77051",
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+      _markers.add(Marker(
+        markerId: MarkerId("Kashmere Multi-Service Center"),
+        position: const LatLng(29.8042647,-95.31655),
+        infoWindow: InfoWindow(
+          title:"Kashmere Multi-Service Center",
+          snippet: "4802 Lockwood Dr 77026",
         ),
         icon: BitmapDescriptor.defaultMarker,
       ));
     });
   }
-
+  void _onCameraMove(CameraPosition position){
+    _lastMapPosition = position.target;
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Map'),
-          backgroundColor: Colors.blue[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-          markers: _markers,
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+              mapType: _currentMapType,
+              markers: _markers,
+              onCameraMove: _onCameraMove,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Column(
+                  children: <Widget> [
+                    FloatingActionButton(
+                      onPressed: _onMapTypeButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(Icons.map, size: 36.0),
+                    ),
+                    SizedBox(height: 16.0),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
